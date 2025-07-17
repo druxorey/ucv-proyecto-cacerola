5185fc8141cb5d844709a548f61f3cb82753eba9
Druxorey <druxorey@gmail.com>
2025-07-17 01:47:54 -0400

    feat(User): add user type and email fields with updated constructor and accessors

--- src/Model/Entities/User.java
+++ src/Model/Entities/User.java
@@ -3,10 +3,19 @@ package Model.Entities;
 public class User {
 	private String userId;
 	private String password;
+	private int type;
+	private String email;
 
 	public User(String userId, String password) {
 		this.userId = userId;
 		this.password = password;
+	}
+
+	public User(String userId, String password, int type, String email) {
+		this.userId = userId;
+		this.password = password;
+		this.type = type;
+		this.email = email;
 	}
 
 	public String getUserId() {
@@ -15,5 +24,13 @@ public class User {
 
 	public String getPassword() {
 		return password;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public String getEmail() {
+		return email;
 	}
 }
