6b5153d75adcd2774d9c608350699dd9afb3922a
Druxorey <druxorey@gmail.com>
2025-07-17 13:09:48 -0400

    feat(UI): update input field styles and restructure login/register views for improved layout

--- src/View/Common/UIStyles.java
+++ src/View/Common/UIStyles.java
@@ -5,9 +5,9 @@ import java.awt.*;
 public class UIStyles {
     // Colors
     public static final Color BG_PRIMARY_COLOR = Color.decode("#fffcf2");
-    public static final Color BG_SECONDARY_COLOR = Color.decode("#ccc5b9");
-    public static final Color FG_PRIMARY_COLOR = Color.decode("#403d39");
-    public static final Color FG_SECONDARY_COLOR = Color.decode("#252422");
+    public static final Color BG_SECONDARY_COLOR = Color.decode("#403d39");
+    public static final Color FG_PRIMARY_COLOR = Color.decode("#252422");
+    public static final Color FG_SECONDARY_COLOR = Color.decode("#ccc5b9");
     public static final Color ACCENT_COLOR = Color.decode("#eb5e28");
 	public static final Color DEBUG_COLOR = Color.decode("#00ff00");
 
@@ -17,6 +17,10 @@ public class UIStyles {
     public static final int PANEL_PADDING = 30;
     public static final int VERTICAL_STRUT_SMALL = 10;
     public static final int VERTICAL_STRUT_MEDIUM = 20;
+    public static final int WINDOW_WIDTH_LOGIN = 820;
+    public static final int WINDOW_HEIGHT_LOGIN = 570;
+    public static final int WINDOW_WIDTH_INTERFACE = 1000;
+    public static final int WINDOW_HEIGHT_INTERFACE = 600;
 
     // Fonts
     public static final Font MAIN_FONT = new Font("Segoe UI", Font.BOLD, 14);
